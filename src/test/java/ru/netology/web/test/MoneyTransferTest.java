1106b8e8c9dc142709492cf5e4313270be26ca9f
Diana Krapivina <mr.ikoma@mail.ru>
2021-06-20 16:27:53 +0300

    Initial commit

---
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -0,0 +1,22 @@
+package ru.netology.web.test;
+
+import org.junit.jupiter.api.Test;
+import ru.netology.web.data.DataHelper;
+import ru.netology.web.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+
+class MoneyTransferTest {
+
+  @Test
+  void shouldTransferMoneyBetweenOwnCardsV2() {
+    open("http://localhost:9999");
+    var loginPage = new LoginPage();
+//    var loginPage = open("http://localhost:9999", LoginPageV2.class);
+    var authInfo = DataHelper.getAuthInfo();
+    var verificationPage = loginPage.validLogin(authInfo);
+    var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+    verificationPage.validVerify(verificationCode);
+  }
+}
+
